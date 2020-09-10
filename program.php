75ac0fd8776d7a9dcb296b1b3443655c42033c6f
Joshua Law <jlaw@warren.k12.in.us>
2020-09-10 08:15:05 -0400

    Changed main home page

--- program.php
+++ program.php
@@ -27,7 +27,7 @@
                 <div class="collapse navbar-collapse" id="collapsibleNavbar">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="index.html">Home</a>
+                        <a class="nav-link" href="../index.html">Home</a>
                     </li>
                     <li class="nav-item active">
                         <a class="nav-link" href="#">Programs</a>
