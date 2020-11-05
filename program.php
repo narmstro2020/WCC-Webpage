43afad7ce9f886f64b40cb649bd0762410156c86
cbSteimel <58703325+cbSteimel@users.noreply.github.com>
2020-11-05 14:41:32 -0500

    analytics code

--- program.php
+++ program.php
@@ -1,6 +1,17 @@
+
+</script>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-E2Z5MCVLDN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-E2Z5MCVLDN');
+</script>
     <meta charset="UTF-8">
     <title><?php echo $page_title ?></title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
