7459ec4172e28e9ef325f393b221657c608e1674
cbSteimel <58703325+cbSteimel@users.noreply.github.com>
2021-03-25 14:12:57 -0400

    lots and lots of photos

--- programs/architechture.php
+++ programs/architechture.php
@@ -1,15 +1,15 @@
 <?php
-$page_title="";
- $program_title="";
+$page_title="Architecture";
+ $program_title="Architecture";
  $videoId="";
  $replacementImg="";
  $programOverview=""; 
- $classOneTitle="";
- $classOneInfo="";
- $classTwoTitle="";
- $classTwoInfo=""; 
- $classThreeTitle="";
- $classThreeInfo="";
+ $classOneTitle="Computers in Production Design";
+ $classOneInfo="This course covers CAD, Layout and Design, Pre-Engineering concept and Print Reading.  Students will learn how to solve and critically evaluate problems in product design and concepts related to manufacturing and materials processes.";
+ $classTwoTitle="Architectual Drafting and Design I";
+ $classTwoInfo="In this course, students will learn how to make a design sketch and turn it into a technical drawing that can be used to build a home or large commercial structure. Students will also go through the model and presentation development phase for designing buildings."; 
+ $classThreeTitle="Architectual Drafting and Design II";
+ $classThreeInfo="Are you planning for a career in engineering, architecture, or design? In Architectural Drafting II, students will design their own house plans, and product revisions and wor with local companies on drawings that need to be updated.";
  $classFourTitle="";
  $classFourInfo="";
  $galleryImgs=[""];
@@ -18,19 +18,22 @@ $page_title="";
  $certificationsName="";
  $partners="";
  $partnersImg="";
- $partnersName="";
- $dualCredit="";
+ $partnersName="ACE Mentor Program";
+ $dualCredit="Students can earn up to 12 free dual credits Architecture: 
+ 3 in Computers In Production Design, 
+ 6 in Architectural Drafting & Design I and
+ 3 Architectural Drafting & Design II";
  $dualCreditImg="";
  $dualCreditName="";
  $internships="";
  $internshipsImg="";
  $internshipsName="";
- $testimonies=[""];
- $testimoniesName=[""];
+ $testimonies=["I gained experience with the programs that architects use in the business world, and made connections that will help me advance my career through the ACE mentoring program.","Taught me a lot of engineering aspects. How to do certain things concerning engineering and architectural designs. I worked in CAD and all sorts of things. Helped me boost my knowledge to become an Architectural Engineer. And since I was in engineering I learned things that a normal Architect wouldn't and that's amazing. Never know if you would want to switch fields or electrical problems can show up while designing a building. ","I learned how to handle certain situations better. How to do better under pressure, and how to be the best at everything I do."];
+ $testimoniesName=["Hannah Jones","Jeremiah Davis","Andrew Gonzalez"];
  $extraImgs=[""];
- $grade1="";
- $grade2="";
- $grade3="";
+ $grade1="Sophomore";
+ $grade2="Junior";
+ $grade3="Senior";
  $grade4="";
  $teacher1="";
  $teacher2="";
@@ -38,7 +41,7 @@ $page_title="";
  $teacher4="";
  $prerequisites1="";
  $prerequisites2="";
- $prerequisites3="";
+ $prerequisites3="Architectural Drafting & Design I ";
  $prerequisites4="";
  
  
