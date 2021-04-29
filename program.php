6a2e7362e4dc4feb096982d9658f6a1fc60bf1c2
jlaw <jlaw@warren.k12.in.us>
2021-04-29 13:54:27 -0400

    restored google

--- program.php
+++ program.php
@@ -4,14 +4,14 @@
 <html lang="en">
 <head>
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<!--<script async src="https://www.googletagmanager.com/gtag/js?id=G-E2Z5MCVLDN"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-E2Z5MCVLDN"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-E2Z5MCVLDN');
-</script>-->
+</script>
     <meta charset="UTF-8">
     <title><?php echo $page_title ?></title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
